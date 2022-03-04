9e1e18e953021a4770331b709f5d7552c6842a7c
Tod Olson <tod@uchicago.edu>
2022-03-04 12:33:34 -0600

    Basic template functionality: args and config file

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Python_template
-Template for Python scripts
+This is a simple template for Python scripts, to handle some basic boilerplate structure.
+
+## Configuration
+This template is set up for both command-line arguments and config file.
+If either are not wanted, it should be easy to remove.
+
+If this is an interactive script where you want prompted for command line arguments, the `argparse-prompt` package is a drop-in wrapper around for `argparse`, see https://pypi.org/project/argparse-prompt/
---
+++ main.py
@@ -0,0 +1,58 @@
+import argparse
+import configparser
+import logging
+import sys
+from email.policy import default
+
+
+def read_config(filename):
+    """Parse the named config file and return an config object"""
+
+    config = configparser.ConfigParser()
+    config.read(filename)
+    return config
+
+
+def parse_args():
+    """Parse command line arguments and return a Namespace object."""
+
+    parser = argparse.ArgumentParser(
+        description=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter
+    )
+    parser.add_argument(
+        "-i",
+        "--infile",
+        help="Input file",
+        default=sys.stdin,
+        type=argparse.FileType("r"),
+    )
+    parser.add_argument(
+        "-o",
+        "--outfile",
+        help="Output file",
+        default=sys.stdout,
+        type=argparse.FileType("w"),
+    )
+    parser.add_argument(
+        "-C", "--config_file", help="Name of config file", default="config.ini"
+    )
+    parser.add_argument(
+        "-v", "--verbose", action="count", default=0, help="Increase verbosity level"
+    )
+    return parser.parse_args()
+
+
+def main():
+    args = parse_args()
+    config = read_config(args.config_file)
+    # Logic or function to override config values from the command line arguments would go here
+
+    return 0
+
+
+if __name__ == "__main__":
+    try:
+        sys.exit(main())
+    except KeyboardInterrupt:
+        print("Interrupted")
+        sys.exit(0)
